c36c4ced373799c26e699003fc6a186e64e2a627
Matheus Abreu <abreumatheus@icloud.com>
2020-05-10 00:57:55 -0300

    Componentes - Importando componentes localmente

---
+++ 7-Introducao-Componentes/components-intro/src/components/Counters.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="counters">
+        <counter-tag></counter-tag>
+        <counter-tag></counter-tag>
+        <counter-tag></counter-tag>
+    </div>
+</template>
+
+<script>
+    import Counter from "./Counter";
+
+    export default {
+        name: "Counters",
+        components: {
+            'counter-tag': Counter
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
