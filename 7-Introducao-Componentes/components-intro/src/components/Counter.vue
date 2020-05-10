9d0d1bb3346b6fba6e8cba8a0bd7a28ee96cf94e
Matheus Abreu <abreumatheus@icloud.com>
2020-05-10 00:51:17 -0300

    Componentes - Primeiro componente com Vue CLI

---
+++ 7-Introducao-Componentes/components-intro/src/components/Counter.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="counter">
+        <h1>Counter: {{ counterStatus }}</h1>
+        <button @click="addToCounter()">Add 1</button>
+        <button @click="subFromCounter()">Sub 1</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Counter",
+        data() {
+            return {
+                counterStatus: 0
+            }
+        },
+        methods: {
+            addToCounter() {
+                this.counterStatus++
+            },
+            subFromCounter() {
+                this.counterStatus--
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ 7-Introducao-Componentes/components-intro/src/main.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import App from './App.vue'
+import Counter from "./components/Counter";
+
+Vue.config.productionTip = false
+Vue.component('counter', Counter)
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
