34ca3d9f2cf53ecbb423c74da6ed247ebbf4fc1a
Matheus Abreu <abreumatheus@icloud.com>
2020-05-10 01:15:29 -0300

    Componentes - Desafio "Componentizar" App

---
+++ 7-Introducao-Componentes/componente-desafio/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-app>
+        <nav-drawer></nav-drawer>
+
+        <toolbar></toolbar>
+
+        <v-content>
+            <carousel></carousel>
+        </v-content>
+
+        <app-footer></app-footer>
+    </v-app>
+</template>
+
+<script>
+    import NavDrawer from "./components/NavDrawer";
+    import Toolbar from "./components/Toolbar";
+    import Carousel from "./components/Carousel";
+    import Footer from "./components/Footer";
+
+    export default {
+        components: {
+            'nav-drawer': NavDrawer,
+            'toolbar': Toolbar,
+            'carousel': Carousel,
+            'app-footer': Footer
+        }
+    }
+</script>
